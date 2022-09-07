e5c10058f9220480b81e20b6c227077d6e4bac1d
unknown <wizard10.vt@gmail.com>
2022-09-07 19:58:13 +0300

    env

--- src/github-api/index.ts
+++ src/github-api/index.ts
@@ -1,6 +1,5 @@
 import { Octokit } from 'octokit';
 
 export const octokit = new Octokit({
-	// todo: token to ENV
-	auth: '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',
+	auth: process.env.GITHUB_API_TOKEN,
 });
--- src/github-api/requests.ts
+++ src/github-api/requests.ts
@@ -5,6 +5,23 @@ import { octokit } from '.';
 
 export const searchUsersByName = async (username: string) => {
 	setIsLoading(true);
+
+	// to get reposetories count we need to fetch users all reposetories(because public_repos is not returned by server, but it should via docs), which too long.
+
+	// const getUsers = async () => {
+	// 	return (await octokit.request('GET /search/users', { q: `${username} in:user` })).data.items[0];
+	// };
+	// type Item = Awaited<Promise<PromiseLike<ReturnType<typeof getUsers>>>> & { repos_count?: number };
+	// type Res = {
+	// 	data: {
+	// 		items: Item[];
+	// 		total_count: number;
+	// 		incomplete_results: boolean;
+	// 	};
+	// 	url: string;
+	// 	headers: any;
+	// 	status: 200;
+	// };
 	const res = await octokit.request('GET /search/users', { q: `${username} in:user` });
 	setIsLoading(false);
 	return res.data.items;
@@ -22,6 +39,7 @@ export const searchUserRepositories = async (username: string, repoName: string 
 	const res = await octokit.request('GET /search/repositories', {
 		q: q + `user:${username}`,
 	});
+
 	setLoadingRepo(false);
 
 	return res.data.items;
